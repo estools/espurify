44be00c71d5886b6a9d2cce58a0d984efa8514f6
Takuto Wada <takuto.wada@gmail.com>
2014-07-22 18:04:14 +0900

    Reformat first test

--- test/test.js
+++ test/test.js
@@ -2,69 +2,73 @@ var espurify = require('..'),
     esprima = require('esprima'),
     assert = require('assert');
 
-it('eliminate extra properties from AST output', function () {
-    var ast = esprima.parse('assert(foo)', {tolerant: true, tokens: true});
-    assert.deepEqual(ast, {
-        type: 'Program',
-        body: [
-            {
-                type: 'ExpressionStatement',
-                expression: {
-                    type: 'CallExpression',
-                    callee: {
-                        type: 'Identifier',
-                        name: 'assert'
-                    },
-                    arguments: [
-                        {
-                            type: 'Identifier',
-                            name: 'foo'
-                        }
-                    ]
-                }
-            }
-        ],
-        tokens: [
-            {
-                type: 'Identifier',
-                value: 'assert'
-            },
-            {
-                type: 'Punctuator',
-                value: '('
-            },
-            {
-                type: 'Identifier',
-                value: 'foo'
-            },
-            {
-                type: 'Punctuator',
-                value: ')'
-            }
-        ],
-        errors: []
-    });
+describe('eliminate extra properties from AST output', function () {
 
-    var purified = espurify(ast);
-    assert.deepEqual(purified, {
-        type: 'Program',
-        body: [
-            {
-                type: 'ExpressionStatement',
-                expression: {
-                    type: 'CallExpression',
-                    callee: {
-                        type: 'Identifier',
-                        name: 'assert'
-                    },
-                    arguments: [
-                        {
+    it('eliminate tokens', function () {
+        var ast = esprima.parse('assert(foo)', {tolerant: true, tokens: true});
+        var purified = espurify(ast);
+
+        assert.deepEqual(ast, {
+            type: 'Program',
+            body: [
+                {
+                    type: 'ExpressionStatement',
+                    expression: {
+                        type: 'CallExpression',
+                        callee: {
                             type: 'Identifier',
-                            name: 'foo'
-                        }
-                    ]
+                            name: 'assert'
+                        },
+                        arguments: [
+                            {
+                                type: 'Identifier',
+                                name: 'foo'
+                            }
+                        ]
+                    }
                 }
-            }
-        ]
+            ],
+            tokens: [
+                {
+                    type: 'Identifier',
+                    value: 'assert'
+                },
+                {
+                    type: 'Punctuator',
+                    value: '('
+                },
+                {
+                    type: 'Identifier',
+                    value: 'foo'
+                },
+                {
+                    type: 'Punctuator',
+                    value: ')'
+                }
+            ],
+            errors: []
+        });
+
+        assert.deepEqual(purified, {
+            type: 'Program',
+            body: [
+                {
+                    type: 'ExpressionStatement',
+                    expression: {
+                        type: 'CallExpression',
+                        callee: {
+                            type: 'Identifier',
+                            name: 'assert'
+                        },
+                        arguments: [
+                            {
+                                type: 'Identifier',
+                                name: 'foo'
+                            }
+                        ]
+                    }
+                }
+            ]
+        });
     });
 });
