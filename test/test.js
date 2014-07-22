33a1d175c421379e33c55c0738bc033e6eb2dfaf
Takuto Wada <takuto.wada@gmail.com>
2014-07-22 18:09:06 +0900

    Eliminate loc test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,29 @@ var espurify = require('..'),
     assert = require('assert');
 
 describe('eliminate extra properties from AST output', function () {
+    beforeEach(function () {
+        this.expected = {
+            type: 'Program',
+            body: [
+                {
+                    type: 'ExpressionStatement',
+                    expression: {
+                        type: 'CallExpression',
+                        callee: {
+                            type: 'Identifier',
+                            name: 'assert'
+                        },
+                        arguments: [
+                            {
+                                type: 'Identifier',
+                                name: 'foo'
+                            }
+                        ]
+                    }
+                }
+            ]
+        };
+    });
 
     it('eliminate tokens', function () {
         var ast = esprima.parse('assert(foo)', {tolerant: true, tokens: true});
@@ -49,7 +72,13 @@ describe('eliminate extra properties from AST output', function () {
             errors: []
         });
 
-        assert.deepEqual(purified, {
+        assert.deepEqual(purified, this.expected);
+    });
+
+    it('eliminate loc', function () {
+        var ast = esprima.parse('assert(foo)', {tolerant: true, loc: true});
+        var purified = espurify(ast);
+        assert.deepEqual(ast, {
             type: 'Program',
             body: [
                 {
@@ -58,17 +87,71 @@ describe('eliminate extra properties from AST output', function () {
                         type: 'CallExpression',
                         callee: {
                             type: 'Identifier',
-                            name: 'assert'
+                            name: 'assert',
+                            loc: {
+                                start: {
+                                    line: 1,
+                                    column: 0
+                                },
+                                end: {
+                                    line: 1,
+                                    column: 6
+                                }
+                            }
                         },
                         arguments: [
                             {
                                 type: 'Identifier',
-                                name: 'foo'
+                                name: 'foo',
+                                loc: {
+                                    start: {
+                                        line: 1,
+                                        column: 7
+                                    },
+                                    end: {
+                                        line: 1,
+                                        column: 10
+                                    }
+                                }
                             }
-                        ]
+                        ],
+                        loc: {
+                            start: {
+                                line: 1,
+                                column: 0
+                            },
+                            end: {
+                                line: 1,
+                                column: 11
+                            }
+                        }
+                    },
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 0
+                        },
+                        end: {
+                            line: 1,
+                            column: 11
+                        }
                     }
                 }
-            ]
+            ],
+            loc: {
+                start: {
+                    line: 1,
+                    column: 0
+                },
+                end: {
+                    line: 1,
+                    column: 11
+                }
+            },
+            errors: []
         });
+
+        assert.deepEqual(purified, this.expected);
     });
+
 });
